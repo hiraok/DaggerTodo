efd2a0c8917539b46ae8da5a900dd8aa9fdcfa64
hiraok <khirao.dev.work@gmail.com>
2020-11-27 00:13:55 +0900

    Api修正

--- app/src/main/java/com/hiraok/chobit_casting/api/TwitCastingApi.kt
+++ app/src/main/java/com/hiraok/chobit_casting/api/TwitCastingApi.kt
@@ -2,6 +2,8 @@ package com.hiraok.chobit_casting.api
 
 import com.hiraok.chobit_casting.api.response.LiveResponse
 import com.hiraok.chobit_casting.api.response.SearchLiveResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import retrofit2.Retrofit
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,10 +13,14 @@ class TwitCastingApi @Inject constructor(
     retrofit: Retrofit
 ) {
     private val service = retrofit.create(TwitCastingService::class.java)
-    suspend fun movies(): List<LiveResponse> = service.movies().movies
+
+    suspend fun movies(): Flow<List<LiveResponse>> = flow { service.movies().movies }
 }
 
 interface TwitCastingService {
     @GET("/search/lives")
-    suspend fun movies(@Query("type") type: String = "recommend", @Query("lang") lang: String = "ja"): SearchLiveResponse
+    suspend fun movies(
+        @Query("type") type: String = "recommend",
+        @Query("lang") lang: String = "ja"
+    ): SearchLiveResponse
 }
