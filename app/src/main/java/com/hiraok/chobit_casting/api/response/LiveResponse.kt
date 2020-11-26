a21ef37e5365a5f78ace1666a4837d600bc2cfa6
hiraok <khirao.dev.work@gmail.com>
2020-11-27 00:14:17 +0900

    Response修正

--- app/src/main/java/com/hiraok/chobit_casting/api/response/LiveResponse.kt
+++ app/src/main/java/com/hiraok/chobit_casting/api/response/LiveResponse.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class LiveResponse(
     @Json(name = "movie") val movie: MovieResponse,
     @Json(name = "broadcaster") val broadcaster: UserResponse,
-    @Json(name = "tags") val tags: Array<String>
+    @Json(name = "tags") val tags: List<String>
 )
