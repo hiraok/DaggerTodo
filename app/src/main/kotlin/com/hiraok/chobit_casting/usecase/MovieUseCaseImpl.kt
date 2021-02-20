d3f708f829121de7d778e4b790ff1ceb4bcf8302
hiraok <khirao.dev.work@gmail.com>
2021-02-20 22:52:20 +0900

    fiexd usecase

--- app/src/main/kotlin/com/hiraok/chobit_casting/usecase/MovieUseCaseImpl.kt
+++ app/src/main/kotlin/com/hiraok/chobit_casting/usecase/MovieUseCaseImpl.kt
@@ -4,6 +4,8 @@ import com.hiraok.chobit_casting.domain.GetMovieListUseCase
 import com.hiraok.chobit_casting.domain.Movie
 import com.hiraok.chobit_casting.domain.MovieRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetMovieListUseCaseImpl @Inject constructor(
